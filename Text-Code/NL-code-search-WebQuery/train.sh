0de785306d40cc39dcad09541980738b781a9106
张瑀晖 <zhangyuhui.666@bytedance.com>
2022-02-07 11:32:32 +0800

    mod

--- Text-Code/NL-code-search-WebQuery/train.sh
+++ Text-Code/NL-code-search-WebQuery/train.sh
@@ -3,13 +3,13 @@ nohup python code/run_classifier.py \
 	--do_train \
 	--do_eval \
 	--eval_all_checkpoints \
-	--train_file train_staqc_3.json \
+	--train_file train_staqc_2.json \
 	--dev_file valid_staqc_0.json \
 	--max_seq_length 200 \
 	--per_gpu_train_batch_size 8 \
 	--per_gpu_eval_batch_size 8 \
 	--learning_rate 5e-5 \
-	--num_train_epochs 5 \
+	--num_train_epochs 10 \
 	--gradient_accumulation_steps 1 \
 	--warmup_steps 1000 \
 	--save_steps 1000 \
