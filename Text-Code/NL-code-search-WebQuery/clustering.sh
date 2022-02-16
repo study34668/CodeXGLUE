0d83824521877a077f16683e1d8ada14d041d8ad
张瑀晖 <zhangyuhui.666@bytedance.com>
2022-02-16 17:04:59 +0800

    mod

--- Text-Code/NL-code-search-WebQuery/clustering.sh
+++ Text-Code/NL-code-search-WebQuery/clustering.sh
@@ -1,9 +1,12 @@
 python code/clustering.py \
 	--model_type roberta \
-	--clustering_file test_staqc_0.json \
+	--clustering_file query_staqc_0.json \
 	--max_seq_length 200 \
 	--per_gpu_batch_size 8 \
 	--data_dir ./data \
 	--output_dir ./model_staqc \
 	--encoder_name_or_path microsoft/codebert-base \
-	--pred_model_dir checkpoint-best-aver
+	--pred_model_dir checkpoint-best-aver \
+	--do_clustering 2>&1 | tee log/clustering.log &
+
+jobs -l > log/pid.log
