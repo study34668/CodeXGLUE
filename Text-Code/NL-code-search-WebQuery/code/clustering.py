9959b334a20c35ba5fab64dc38bed4b95f856c33
张瑀晖 <zhangyuhui.666@bytedance.com>
2022-02-17 17:56:13 +0800

    fix

--- Text-Code/NL-code-search-WebQuery/code/clustering.py
+++ Text-Code/NL-code-search-WebQuery/code/clustering.py
@@ -119,12 +119,11 @@ def prepare_test_json(args, model, tokenizer):
     best_idx = -1
     best_logit = 0.0
     for idx, result in enumerate(results):
-        print(nl_vec.size())
         code_vec = torch.tensor(result['cluster_center']).unsqueeze(0)
-        print(code_vec.size())
-        logits, _, _ = model(code_vec, nl_vec, labels, use_input=True)
-        logit = logits.squeeze().numpy().tolist()[0]
-        print(logit)
+        with torch.no_grad():
+            logits, _, _ = model(code_vec, nl_vec, labels, use_input=True)
+            logit = float(logits.squeeze().numpy().tolist()[0])
+            print(logit)
         if logit > best_logit:
             best_logit = logit
             best_idx = idx
