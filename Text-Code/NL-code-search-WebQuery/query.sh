1aebe31a605a4f52c8185ab06c01e134371831cb
张瑀晖 <zhangyuhui.666@bytedance.com>
2022-02-21 16:25:18 +0800

    mod

--- Text-Code/NL-code-search-WebQuery/query.sh
+++ Text-Code/NL-code-search-WebQuery/query.sh
@@ -25,6 +25,10 @@ python code/run_classifier.py \
 	--prediction_file ./evaluator/staqc_query_predictions.txt
 
 python code/clustering.py \
+  --model_type roberta \
+  --encoder_name_or_path microsoft/codebert-base \
+  --max_seq_length 200 \
+	--per_gpu_batch_size 8 \
 	--data_dir ./data \
   --output_dir ./model_staqc \
 	--pred_model_dir checkpoint-best-aver \
