267dd9ec14db18b2ac393c2cce63e0e8c704dc07
张瑀晖 <zhangyuhui.666@bytedance.com>
2022-02-21 18:22:52 +0800

    modL :

--- Text-Code/NL-code-search-WebQuery/train.sh
+++ Text-Code/NL-code-search-WebQuery/train.sh
@@ -8,10 +8,10 @@ nohup python code/run_classifier.py \
 	--max_seq_length 200 \
 	--per_gpu_train_batch_size 8 \
 	--per_gpu_eval_batch_size 8 \
-	--learning_rate 5e-5 \
+	--learning_rate 1e-5 \
 	--num_train_epochs 5 \
 	--gradient_accumulation_steps 1 \
-	--warmup_steps 2000 \
+	--warmup_steps 1000 \
 	--save_steps 2000 \
 	--evaluate_during_training \
 	--data_dir ./data/ \
